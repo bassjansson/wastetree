71b5b6d049aea5adf8485c7c85de7825f5ab82c1
Bass Jansson <bassjansson@gmail.com>
2018-05-17 14:33:20 +0200

    Improves sensors arduino code.

--- sensors/sensors.ino
+++ sensors/sensors.ino
@@ -1,27 +1,40 @@
 
-#define SENSOR_PIN 7
+#define SENSOR_PIN_1 4
+#define SENSOR_PIN_2 7
+#define SENSOR_PIN_3 8
 
-int numOfCups = 0;
+bool sensorRead1 = false;
+bool sensorRead2 = false;
+bool sensorRead3 = false;
+
+void printSensorTrigger(int sensor)
+{
+    Serial.print(sensor);
+    Serial.println(",1");
+
+    delay(250);
+}
 
 void setup()
 {
     Serial.begin(9600);
-    pinMode(SENSOR_PIN, INPUT);
+
+    pinMode(SENSOR_PIN_1, INPUT);
+    pinMode(SENSOR_PIN_2, INPUT);
+    pinMode(SENSOR_PIN_3, INPUT);
 }
- 
+
 void loop()
 {
-    // First wait till the sensor picked up something
-    // Then wait till the sensor is clear again
-    while (digitalRead(SENSOR_PIN));
-    while (!digitalRead(SENSOR_PIN));
+    bool sr1 = !digitalRead(SENSOR_PIN_1);
+    bool sr2 = !digitalRead(SENSOR_PIN_2);
+    bool sr3 = !digitalRead(SENSOR_PIN_3);
 
-    // Print to serial port and increment number of cups
-    Serial.print("Bekertje ging erin! Totaal: ");
-    Serial.println(numOfCups);
-    numOfCups++;
+    if (sr1 && !sensorRead1) printSensorTrigger(1);
+    if (sr2 && !sensorRead2) printSensorTrigger(2);
+    if (sr3 && !sensorRead3) printSensorTrigger(3);
 
-    // Delay half a second
-    delay(500);
+    sensorRead1 = sr1;
+    sensorRead2 = sr2;
+    sensorRead3 = sr3;
 }
-
